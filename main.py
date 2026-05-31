013c099430e812b9ee6817be66eb41ef8e43c899
AtanasovCode <a_ljupce@yahoo.com>
2026-05-31 19:11:49 +0200

    include cart router & create db debug route

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from contextlib import asynccontextmanager
+from sqlalchemy.orm import Session
+from database.database import get_db
 from database.database import engine, Base
 from database.seed import seed
+from models.cart import Cart
+from models.user import User
+import models
+
 # Routers
 from web.book_router import router as book_api_router
 from web.category_router import router as category_api_router
@@ -33,3 +39,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/debug/db")
+async def debug(db: Session = Depends(get_db)):
+    return {
+        "users": db.query(User).all(),
+        "carts": db.query(Cart).all()
+    }
